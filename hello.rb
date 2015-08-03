7ff17795836bd5ecd42e06e41f94b40200c805fd
Ajitsy <sanghera.p@gmail.com>
2015-08-03 12:18:47 +0100

    modified 'hello' string with HTML

--- hello.rb
+++ hello.rb
@@ -1,12 +1,14 @@
 require 'sinatra'
 
-configure :development do   
-  set :bind, '0.0.0.0'   
+configure :development do
+  set :bind, '0.0.0.0'
   set :port, 3000 # Not really needed, but works well with the "Preview" menu option
 end
 
 get '/' do
-  'hello!'
+  "<div>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
 
 get '/secret' do
