218abcf1644286eac3d562aaf7abc2a1275782b6
Richard Charman <richardcharman@live.com>
2015-08-03 11:09:34 +0000

    Added secret page

--- hello.rb
+++ hello.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
 
+configure :development do   
+  set :bind, '0.0.0.0'   
+  set :port, 3000 # Not really needed, but works well with the "Preview" menu option
+end
+
 get '/' do
   'hello!'
+end
+
+get '/secret' do
+  'this is a secret page'
 end
